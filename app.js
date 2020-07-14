bb0af12b4421b2c3e92a6d16cf0f26f0e8905c72
brian presta <brian.presta@gmail.comgit commit -m first>
2020-07-14 11:35:20 -0700

    set up basic inquirer question

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var fs = require('fs')
-const generatePage = require('./src/page-template.js')
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name,github] = profileDataArgs;
+// var fs = require('fs')
+var inquirer = require("inquirer")
+// var generatePage = require('./src/page-template.js')
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// var [name,github] = profileDataArgs;
 
-console.log(generatePage(name,github))
-fs.writeFile('index.html',generatePage(name,github),function(){return 'oops'})
+// console.log(generatePage(name,github))
+// fs.writeFile('index.html',generatePage(name,github),function(){return 'oops'})
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers))
