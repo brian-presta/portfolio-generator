3d15ed066fbe8d13eb49fb928c25999d20a26157
brian presta <brian.presta@gmail.comgit commit -m first>
2020-07-15 17:06:04 -0700

    set up promises

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,1 +1,31 @@
-const fs = require('fs')
+const fs = require('fs');
+const { resolve } = require('path');
+
+const writeFile = fileContent => {
+    return new Promise((resolve,reject) => {
+        fs.writeFile('./dist/index.html',fileContent,function(err) {
+            if (err) {
+              reject(err)
+              return
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            })
+        })
+    })
+}
+const copyFile = function() {
+    return new Promise(function(resolve,reject) {
+        fs.copyFile('./src/style.css','./dist/style.css',err => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve({
+                ok:true,
+                message: 'File copied!'
+            })
+        })
+    })
+}
